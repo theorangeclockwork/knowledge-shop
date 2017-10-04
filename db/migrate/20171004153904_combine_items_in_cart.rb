d494fd50fa9f2b7b6d7268bd10bc9750de69f086
Ruslan Safin <AlpientGould@gmail.com>
2017-10-04 19:15:15 +0300

    fix

--- db/migrate/20171004153904_combine_items_in_cart.rb
+++ db/migrate/20171004153904_combine_items_in_cart.rb
@@ -20,7 +20,7 @@ class CombineItemsInCart < ActiveRecord::Migration[5.1]
     def down
         LineItem.where("quantity>1").each do |line_item|
             line_item.quantity.times do
-                LineItem.create cart_id: line_item.cart_id
+                LineItem.create cart_id: line_item.cart_id,
                     product_id: line_item.product_id, quantity: 1
             end
 
