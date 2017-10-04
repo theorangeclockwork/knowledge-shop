f72f532dcff3f91f6886dc22e85619d5c770b94c
Ruslan Safin <AlpientGould@gmail.com>
2017-10-05 00:08:53 +0300

    end-up cart, little css update

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -30,10 +30,15 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 #    assert_redirected_to line_item_url(LineItem.last)
  # end
 
-  test "should show line_item" do
-    get line_item_url(@line_item)
-    assert_response :success
-  end
+  # test "should show line_item" do
+#    get line_item_url(@line_item)
+ #   assert_response :success
+#  end
+
+test "should update line_item" do
+    patch :update, id: @line_item, line_item: { product_id: @line_item.product_id }
+    assert_redirected_to line_item_path(assigns(:line_item))
+end
 
   test "should get edit" do
     get edit_line_item_url(@line_item)
